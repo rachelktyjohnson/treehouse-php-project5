72d082c3b0e60a297ee3c6ba0ec32840eafd27b4
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-05-02 15:14:24 +1000

    :hammer: Meets

---
+++ resources/views/dogs.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+
+@include('head')
+
+<!-- START body -->
+<body style="background-color: #4d4d4d !important">
+
+    @include('nav')
+
+    <!-- START main -->
+    <main role="main" class="container">
+        <div class="row mb-4">
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,1" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,2" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,3" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end row -->
+        <div class="row mb-4">
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,4" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,5" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,6" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end row -->
+        <div class="row mb-4">
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,7" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,8" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card bg-dark">
+                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,puppy,9" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title text-light">Pet name</h5>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end row -->
+    </main><!-- END main -->
+
+        @include('footer')
+
+</body> <!-- END Body -->
+</html>
